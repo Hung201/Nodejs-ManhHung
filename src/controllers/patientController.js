2b7fa265430e5c5ab62a2111f7d889460fa386cb
Hung Pham <hungphammanh2012003@gmail.com>
2024-02-22 11:33:28 +0700

    87p2

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -4,13 +4,13 @@ let postBookAppointment = async (req, res) => {
     try {
         let infor = await patientServices.postBookAppointment(req.body);
         return res.status(200).json(
-            infor
+            infor.res
         )
     } catch (e) {
         console.log(e)
         return res.status(200).json({
             errCode: -1,
-            errMessage: 'Error from the severhh'
+            errMessage: 'Error from the sever'
         })
     }
 }
--- src/services/patientService.js
+++ src/services/patientService.js
@@ -5,73 +5,84 @@ import emailService from './emailService'
 let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.email || !data.doctorId || !data.timeType || !data.date || !data.fullName) {
+            if (!data.email || !data.doctorId || !data.timeType || !data.date || !data.fullName || !data.birthday
+                || !data.selectedGender || !data.reason || !data.address || !data.phoneNumber
+            ) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing parameter'
                 })
             } else {
-                await emailService.sendSimpleEmail({
-                    reciverEmail: data.email,
-                    patientName: data.fullName,
-                    time: data.timeString,
-                    doctorName: data.doctorName,
-                    language: data.language,
-                    redirectLink: 'https://www.youtube.com/watch?v=puA-NnjVuiY&t=623s'
-                })
+                let res = {};
+
                 //update patient
-                let user = await db.User.findOrCreate({
+                let user = await db.User.findOne({
                     where: { email: data.email },
-                    defaults: {
-                        email: data.email,
-                        roleId: 'R3'
-                    },
                 });
 
                 // find latest entry
-                if (user && user[0]) {
+                if (user && user.email) {
                     let eleLatest = await db.Booking.findOne({
                         where: {
-                            patientId: user[0].id
+                            patientId: user.id
                         },
                         order: [['createdAt', 'DESC']],
                     });
                     if (!eleLatest) {
                         await db.Booking.findOrCreate({
-                            where: { patientId: user[0].id },
+                            where: { patientId: user.id },
                             defaults: {
                                 statusId: 'S1',
                                 doctorId: data.doctorId,
-                                patientId: user[0].id,
+                                patientId: user.id,
                                 date: data.date,
                                 timeType: data.timeType
                             }
                         })
-                        resolve({
-                            errCode: 0,
-                            errMessage: 'Save infor patient success!'
+
+                        res.errCode = 0;
+                        res.errMessage = 'Save infor patient success!'
+                        // send email
+                        await emailService.sendSimpleEmail({
+                            reciverEmail: data.email,
+                            patientName: data.fullName,
+                            time: data.timeString,
+                            doctorName: data.doctorName,
+                            language: data.language,
+                            redirectLink: 'https://www.youtube.com/watch?v=puA-NnjVuiY&t=623s'
                         })
                     }
-                    if (eleLatest && eleLatest.statusId === 'S3' || eleLatest.statusId === 'S4') {
+                    else if (eleLatest && eleLatest.statusId === 'S3' || eleLatest.statusId === 'S4') {
                         await db.Booking.create({
                             statusId: 'S1',
                             doctorId: data.doctorId,
-                            patientId: user[0].id,
+                            patientId: user.id,
                             date: data.date,
                             timeType: data.timeType
                         });
-                        resolve({
-                            errCode: 0,
-                            errMessage: 'Save infor patient success!'
+
+                        res.errCode = 0;
+                        res.errMessage = 'Save infor patient success!'
+                        // send email
+                        await emailService.sendSimpleEmail({
+                            reciverEmail: data.email,
+                            patientName: data.fullName,
+                            time: data.timeString,
+                            doctorName: data.doctorName,
+                            language: data.language,
+                            redirectLink: 'https://www.youtube.com/watch?v=puA-NnjVuiY&t=623s'
                         })
                     }
-                    console.log('check ele:', eleLatest)
+                    else if (eleLatest && eleLatest.statusId === 'S1' || eleLatest.statusId === 'S2') {
+                        res.errCode = 2;
+                        res.errMessage = 'Save infor patient failed!'
+                    }
+                } else {
+                    res.errCode = 3;
+                    res.errMessage = `Your email doesn't exist!`
                 }
-
-
-
+                resolve({ res })
             }
-
         } catch (e) {
             reject(e)
         }
