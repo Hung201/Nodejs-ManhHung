cddbd66f9fd67e9ff9cbfbc3d66ec54ee203f913
Hung Pham <hungphammanh2012003@gmail.com>
2024-03-30 18:58:54 +0700

    96

--- src/route/web.js
+++ src/route/web.js
@@ -4,6 +4,7 @@ import userController from "../controllers/userController";
 import doctorController from "../controllers/doctorController"
 import patientController from "../controllers/patientController"
 import specialtyController from "../controllers/specialtyController"
+import clinicController from "../controllers/clinicController"
 
 let router = express.Router();
 
@@ -41,7 +42,9 @@ let initWebRoutes = (app) => {
     router.get('/api/get-all-specialty', specialtyController.getAllSpecialty);
     router.get('/api/get-all-detail-specialty-by-id', specialtyController.getDetailSpecialtyById);
 
-
+    router.post('/api/create-new-clinic', clinicController.createClinic)
+    // router.get('/api/get-all-specialty', specialtyController.getAllSpecialty);
+    // router.get('/api/get-all-detail-specialty-by-id', specialtyController.getDetailSpecialtyById);
 
 
 
