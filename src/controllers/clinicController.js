7c592cdfa186360b52f248c9757a549074f76df3
Hung Pham <hungphammanh2012003@gmail.com>
2024-04-09 22:43:15 +0700

    97

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -15,6 +15,36 @@ let createClinic = async (req, res) => {
     }
 }
 
+let getAllClinic = async (req, res) => {
+    try {
+        let infor = await clinicService.getAllClinic(req.body);
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the sever'
+        })
+    }
+}
+
+let getDetailClinicById = async (req, res) => {
+    try {
+        let infor = await clinicService.getDetailClinicById(req.query.id);
+        return res.status(200).json(
+            infor
+        )
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the sever'
+        })
+    }
+}
+
 module.exports = {
-    createClinic
+    createClinic, getAllClinic, getDetailClinicById
 }
